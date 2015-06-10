9c9cf99746969db02ed58ce24cc5a726c95b2563
Tom Bettany <tom@kano.me>
2015-06-10 10:52:43 +0100

    init: Add module for handling i18n setup
    A module which can import and install gettext (including defining the
    function for deferred translations).
    
    Also adds the Debian packaging for the repository.

---
+++ kano_i18n/init.py
@@ -0,0 +1,17 @@
+#
+# init.py
+#
+# Copyright (C) 2015 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
+#
+# Initialisation utilities for i18n
+#
+
+import gettext
+import __builtin__
+
+def install(app, locale_dir=None):
+    # N_() defined globally for deferred translation
+    __builtin__.__dict__['N_'] = lambda msg: msg
+
+    gettext.install(app, localedir=locale_dir, unicode=1)
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Kano i18n',
+    version='1.0',
+    description='i18n tools for Kano OS',
+    author='Team Kano',
+    author_email='dev@kano.me',
+    url='https://github.com/KanoComputing/kano-i18n',
+    packages=['kano_i18n']
+)
